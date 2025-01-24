dd60932b1d69ae3e6e8b5bdcc07fb56d5114de21
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-01-24 23:42:01 +0200

    props: Set appcompat value of ro.boot.verifiedbootstate to green on non-eng builds
    * at least one app (Revolut) refuses to work with yellow verifiedbootstate
    
    [Terminator-J]: Make it apply to both user & userdebug
    
    Reference: GrapheneOS/platform_build_soong@66530ed

--- core/main.mk
+++ core/main.mk
@@ -394,6 +394,7 @@ ifneq (,$(user_variant))
   # Target is secure in user builds.
   ADDITIONAL_SYSTEM_PROPERTIES += ro.secure=1
   ADDITIONAL_SYSTEM_PROPERTIES += security.perf_harden=1
+  ADDITIONAL_SYSTEM_PROPERTIES += ro.appcompat_override.ro.boot.verifiedbootstate=green
 
   ifeq ($(user_variant),user)
     ADDITIONAL_SYSTEM_PROPERTIES += ro.adb.secure=1
